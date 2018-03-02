4abf0cf13b6dfa95f510946b9b10a074e9ff8000
SpongyBacon <spongybacon@hotmail.co.uk>
2018-03-02 19:41:07 +0000

    add example chart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Chart } from 'react-google-charts'
+import logo from './logo.svg'
+import './App.css'
+
+class ExampleChart extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      options: {
+        title: 'Age vs. Weight comparison',
+        hAxis: { title: 'Age', minValue: 0, maxValue: 15 },
+        vAxis: { title: 'Weight', minValue: 0, maxValue: 15 },
+        legend: 'none',
+      },
+      data: [
+        ['Age', 'Weight'],
+        [8, 12],
+        [4, 5.5],
+        [11, 14],
+        [4, 5],
+        [3, 3.5],
+        [6.5, 7],
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <Chart
+        chartType="ScatterChart"
+        data={this.state.data}
+        options={this.state.options}
+        graph_id="ScatterChart"
+        width="100%"
+        height="400px"
+        legend_toggle
+      />
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <ExampleChart />
+    )
   }
 }
 
-export default App;
+export default App
